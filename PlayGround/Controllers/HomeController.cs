6dfa1a4e558f6567e701234cae6cc70d8888eead
Joseph Phillips <phillipstcllc@gmail.com>
2019-03-29 22:34:48 -0400

    Set up entity framework

--- PlayGround/Controllers/HomeController.cs
+++ PlayGround/Controllers/HomeController.cs
@@ -3,28 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.Data.SqlClient;
+//using PlayGround.Context;
 
 namespace PlayGround.Controllers
 {
+
     public class HomeController : Controller
     {
+        string connectionString;
+
+        //PlayGroundLINQtoSQLClassDataContext dc = new PlayGroundLINQtoSQLClassDataContext(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+
         public ActionResult Index()
         {
+            var connectionManagerDatabaseServers = ConfigurationManager.GetSection("connectionStrings") as NameValueCollection;
+
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+
+
+
+
+                ViewBag.Message = "Your application description page.";
 
             return View();
         }
+
+
+         
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            //ViewBag.Message = "Your contact page.";
+            //DataContexts context = new DataContexts("DefaultConnection");
+
+            //var queryUsers = from FirstName in context.Users
+            //                 select FirstName;
+
+
+            ViewBag.Message = "Your application description page.";
+            
+
+
 
             return View();
         }
+
+        
+
+
     }
 }
