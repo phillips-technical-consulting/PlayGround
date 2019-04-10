e347dca1151de1ada04884f849385b9868768140
Joseph Phillips <phillipstcllc@gmail.com>
2019-04-09 21:25:48 -0400

    general sync

--- PlayGround/Controllers/HomeController.cs
+++ PlayGround/Controllers/HomeController.cs
@@ -15,25 +15,71 @@ namespace PlayGround.Controllers
 
     public class HomeController : Controller
     {
-        private User user = new User();
+        //private User user = new User();
+        private Login login = new Login();
         private SchoolContext db = new SchoolContext();
         string query2;
         //PlayGroundLINQtoSQLClassDataContext dc = new PlayGroundLINQtoSQLClassDataContext(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
 
-        public ActionResult Index()
-        {
-            var queryUsers = user.FirstName;
-            
 
-            var users = db.Users;
-            
-            foreach(User u in users)
+
+        // POST: Users/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login([Bind(Include = "LoginIdTxt,PasswordTxt")] User user)
+        {
+            if (ModelState.IsValid)
             {
-                query2 = u.FirstName;
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
             }
 
-            return View();
+            return View(user);
         }
+
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+
+            return View(login);
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(Login login)
+        {
+
+            ViewBag.Message = login.LoginIdTxt;
+
+            Login(login.LoginIdTxt,login.PasswordTxt);
+
+            return View(login);
+        }
+
+
+
+
+        public void Login(string username, string password)
+        {
+            var users = db.Users;
+            foreach (User u in users)
+            {
+                if (u.LoginIdTxt == username & u.PasswordTxt == password)
+                {
+                    Response.Redirect("~/Home/About");
+                }
+
+
+            }
+
+
+
+        }
+
 
         public ActionResult About()
         {
---
+++ PlayGround/Models/Login.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlayGround.Models
+{
+    public class Login
+    {
+        public string LoginIdTxt { get; set; }
+       
+        public string PasswordTxt { get; set; }
+   
+    }
+}
+
